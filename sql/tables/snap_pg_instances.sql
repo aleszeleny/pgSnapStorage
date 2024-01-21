d099aa495e386dfd775811274ca46931c5a18311
Aleš Zelený <zeleny.ales@gmail.com>
2024-01-21 02:41:12 +0100

    Formattig

--- sql/tables/snap_pg_instances.sql
+++ sql/tables/snap_pg_instances.sql
@@ -103,7 +103,17 @@ GRANT snaps_conn, snaps_ro_cfg, snaps_ro_data, snaps_rw_data TO tu_snaps_storage
 GRANT snaps_conn, snaps_ro_cfg, snaps_ro_data TO tu_snaps_read; -- an application user providing access to stored data
 
 
--- Create schema for snapshot tables
+/*
+********************************************************************************
+* Schemas                                                                      *
+********************************************************************************
+*/
+
+/*
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~ Create schema for snapshot tables                                           ~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
 CREATE SCHEMA IF NOT EXISTS :cfg_ns AUTHORIZATION :snapsdbown;
 REVOKE ALL ON SCHEMA :cfg_ns FROM public;
 GRANT USAGE ON SCHEMA :cfg_ns TO snaps_ro_cfg, snaps_rw_cfg, snaps_ro_data, snaps_rw_data;
@@ -120,9 +130,33 @@ ALTER DEFAULT PRIVILEGES FOR ROLE :snapsdbown
   IN SCHEMA :cfg_ns
   GRANT USAGE ON SEQUENCES TO snaps_rw_cfg;
 
-SET ROLE :snapsdbown;
+/*
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~ Create schema for snapshot data tables                                      ~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+CREATE SCHEMA IF NOT EXISTS :data_ns AUTHORIZATION :snapsdbown;
+REVOKE ALL ON SCHEMA :data_ns FROM public;
+GRANT USAGE ON SCHEMA :data_ns TO snaps_ro_data, snaps_rw_data;
 
-/* Generic trigger functions */
+ALTER DEFAULT PRIVILEGES FOR ROLE :snapsdbown
+  IN SCHEMA :data_ns
+  GRANT SELECT ON TABLES TO snaps_ro_data;
+
+ALTER DEFAULT PRIVILEGES FOR ROLE :snapsdbown
+  IN SCHEMA :data_ns
+  GRANT INSERT, UPDATE, DELETE ON TABLES TO snaps_rw_data;
+
+ALTER DEFAULT PRIVILEGES FOR ROLE :snapsdbown
+  IN SCHEMA :data_ns
+  GRANT USAGE ON SEQUENCES TO snaps_rw_data;
+
+/*
+********************************************************************************
+* Generic trigger function(s)                                                  *
+********************************************************************************
+*/
+SET ROLE :snapsdbown;
 
 CREATE OR REPLACE FUNCTION :cfg_ns.lastmod_tf()
 RETURNS trigger
@@ -132,7 +166,7 @@ BEGIN
 
   IF NEW.lastmod IS NULL THEN
 
-    NEW.lastmod:= now();
+    NEW.lastmod:= pg_catalog.now();
 
   END IF;
 
@@ -140,17 +174,27 @@ BEGIN
 END;
 $function$;
 
-/* PostgreSQL system(s) inventory table */
+/*
+********************************************************************************
+* Configuration tables                                                         *
+********************************************************************************
+*/
+
+/*
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~ PostgreSQL systems inventory table                                           ~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
 CREATE TABLE IF NOT EXISTS :cfg_ns.system (
     system_id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY
   , systemid bigint NOT NULL UNIQUE /* remove unique consstraint in the case of an unexpected real-life systemid collision */
   , system_name text
   , system_description text
-  , lastmod TIMESTAMPTZ NOT NULL DEFAULT now()
+  , lastmod TIMESTAMPTZ NOT NULL DEFAULT pg_catalog.now()
 );
 
 COMMENT ON TABLE :cfg_ns.system IS
-  'Postgresql systems inventory.';
+  'Postgresql system(s) inventory.';
 
 COMMENT ON COLUMN :cfg_ns.system.systemid IS
   'System ID, see pg_control_system().';
@@ -164,13 +208,17 @@ CREATE TRIGGER system_lastmod
   FOR EACH ROW
   EXECUTE FUNCTION :cfg_ns.lastmod_tf();
 
-/* PostgreSQL system(s) relation table */
+/*
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~ PostgreSQL systems tree/history relation table                               ~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
 CREATE TABLE IF NOT EXISTS :cfg_ns.system_tree (
     system_tree_id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY
   , description text
   , ancestor_system_id int NOT NULL REFERENCES :cfg_ns.system(system_id)
   , descendant_system_id int NOT NULL REFERENCES :cfg_ns.system(system_id)
-  , lastmod TIMESTAMPTZ NOT NULL DEFAULT now()
+  , lastmod TIMESTAMPTZ NOT NULL DEFAULT pg_catalog.now()
   , CONSTRAINT system_tree_key UNIQUE (ancestor_system_id, descendant_system_id)
 );
 
@@ -191,7 +239,11 @@ CREATE TRIGGER system_tree_lastmod
   FOR EACH ROW
   EXECUTE FUNCTION :cfg_ns.lastmod_tf();
 
-/* Postgresql instances inventory table */  
+/*
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~ Postgresql instances inventory table                                         ~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
 CREATE TABLE IF NOT EXISTS :cfg_ns.instance (
     instance_id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY
   , system_id int NOT NULL REFERENCES :cfg_ns.system(system_id)
@@ -200,8 +252,9 @@ CREATE TABLE IF NOT EXISTS :cfg_ns.instance (
   , listen_port integer NOT NULL
   , instance_name text
   , instance_description text
-  , registration_time TIMESTAMPTZ NOT NULL DEFAULT now()
-  , lastmod TIMESTAMPTZ NOT NULL DEFAULT now()
+  , major_version text
+  , registration_time TIMESTAMPTZ NOT NULL DEFAULT pg_catalog.now()
+  , lastmod TIMESTAMPTZ NOT NULL DEFAULT pg_catalog.now()
 );
 
 CREATE INDEX ON :cfg_ns.instance(system_id);
@@ -211,9 +264,6 @@ COMMENT ON TABLE :cfg_ns.instance IS
 
 COMMENT ON COLUMN :cfg_ns.instance.instance_name IS
   'Custom name for the instance (PostgreSQL cluster).';
-
-COMMENT ON COLUMN :cfg_ns.instance.instance_description IS
-  'Instance description.';
 
 
 CREATE OR REPLACE FUNCTION :cfg_ns.instance_defaults_tf()
@@ -250,6 +300,116 @@ CREATE TRIGGER instance_lastmod
   ON :cfg_ns.instance
   FOR EACH ROW
   EXECUTE FUNCTION :cfg_ns.lastmod_tf();
+
+/*
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~ Postgresql databases inventory table                                         ~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+CREATE TABLE IF NOT EXISTS :cfg_ns.database (
+    database_id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY
+  , instance_id int NOT NULL REFERENCES :cfg_ns.instance(instance_id)
+  , collect_cluster boolean NOT NULL DEFAULT true
+  , collect_db boolean NOT NULL DEFAULT true
+  , dbname text NOT NULL
+  , connect_string text NOT NULL
+  , database_description text
+  , registration_time TIMESTAMPTZ NOT NULL DEFAULT pg_catalog.now()
+  , lastmod TIMESTAMPTZ NOT NULL DEFAULT pg_catalog.now()
+);
+
+CREATE INDEX ON :cfg_ns.database(instance_id);
+
+COMMENT ON TABLE :cfg_ns.database IS
+  'Configuration table for source databases.';
+
+COMMENT ON COLUMN :cfg_ns.database.collect_cluster IS
+  'Enable instance (PostgreSQL cluster) wide stats collection.';
+
+COMMENT ON COLUMN :cfg_ns.database.collect_db IS
+  'Enable connected database wide stats collection.';
+
+DROP TRIGGER IF EXISTS database_lastmod
+  ON :cfg_ns.database;
+
+CREATE TRIGGER database_lastmod
+  BEFORE INSERT OR UPDATE
+  ON :cfg_ns.database
+  FOR EACH ROW
+  EXECUTE FUNCTION :cfg_ns.lastmod_tf();
+
+/*
+********************************************************************************
+* Snapshot data tables                                                        *
+********************************************************************************
+*/
+
+/*
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~ Snapshot table                                                               ~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+
+CREATE TABLE IF NOT EXISTS :data_ns.snapshot (
+    snapshot_id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY
+  , snaptime TIMESTAMPTZ NOT NULL DEFAULT pg_catalog.now()
+  , database_id int NOT NULL REFERENCES :cfg_ns.database(database_id)
+  , cluster_stats boolean NOT NULL
+  , db_stats boolean NOT NULL
+  , CONSTRAINT snapshot_key UNIQUE (database_id, snaptime)
+);
+
+COMMENT ON TABLE :data_ns.snapshot IS
+'Snapshot metadata.';
+
+COMMENT ON COLUMN :data_ns.snapshot.snaptime IS
+'Data collection timestamp.';
+
+COMMENT ON COLUMN :data_ns.snapshot.database_id IS
+'Reference to database used to collect snapshot data.';
+
+COMMENT ON COLUMN :data_ns.snapshot.cluster_stats IS
+'Cluster (instance) wide statitic were collected.';
+
+COMMENT ON COLUMN :data_ns.snapshot.db_stats IS
+'Database wide statitic were collected.';
+
+
+/*
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~ pg_settings table                                                   ~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+CREATE TABLE IF NOT EXISTS :data_ns.pg_settings (
+    hash             text NOT NULL PRIMARY KEY
+  , name             text
+  , setting          text
+  , unit             text
+  , category         text
+  , short_desc       text
+  , extra_desc       text
+  , context          text
+  , vartype          text
+  , source           text
+  , min_val          text
+  , max_val          text
+  , enumvals         text[]
+  , boot_val         text
+  , reset_val        text
+  , sourcefile       text
+  , sourceline       integer
+  , pending_restart  boolean
+);
+
+COMMENT ON TABLE :data_ns.pg_settings IS
+E'pg_settings data snapshots.';
+
+CREATE TABLE IF NOT EXISTS :data_ns.snapshot_pg_settings (
+    snapshot_id bigint NOT NULL REFERENCES :data_ns.snapshot(snapshot_id)
+  , hash text NOT NULL REFERENCES :data_ns.pg_settings(hash)
+  , CONSTRAINT snapshot_pg_settings_pkey PRIMARY KEY (snapshot_id, hash)
+);
+
 
 /*
 GRANT SELECT ON ALL TABLES IN SCHEMA :cfg_ns TO snaps_ro_cfg;
